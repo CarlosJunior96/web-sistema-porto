848b27866fecb211f6552569be8704593963394f
Carlos Junior Silva <cjrcordeiro@gmail.com>
2021-03-01 09:15:44 -0300

    alterado server

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${sistemaporto}`));
+app.use(express.static(`${__dirname}/dist/${web-sistema-porto}`));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/${sistemaporto}/index.html`));
+  res.sendFile(path.join(`${__dirname}/dist/${web-sistema-porto}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
